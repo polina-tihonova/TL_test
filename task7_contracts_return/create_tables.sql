26efe8952f15bcf0649b0ab9f64b1811bf116c37
Полина Тихонова <polina@MacBook-Air-Polina.local>
2022-03-21 12:03:39 +0300

    Исправила tabs в spaces

--- task7_contracts_return/create_tables.sql
+++ task7_contracts_return/create_tables.sql
@@ -2,57 +2,57 @@ USE [polinadb_contracts]
 GO
 
 CREATE TABLE [dbo].[Contract](
-	ContractID INT IDENTITY(1,1) NOT NULL,  
-	ContractTypeID INT NOT NULL, 
-	ProductID INT NULL, 
-	Number NVARCHAR(20) NOT NULL, 
-	StartDate DATE NULL, 
-	EndDate DATE NULL
+    ContractID INT IDENTITY(1,1) NOT NULL, 
+    ContractTypeID INT NOT NULL, 
+    ProductID INT NULL, 
+    Number NVARCHAR(20) NOT NULL, 
+    StartDate DATE NULL, 
+    EndDate DATE NULL
 )
 GO
 
 CREATE TABLE [dbo].[ContractType](
-	ContractTypeID INT IDENTITY(1,1) NOT NULL,  
-	TypeName NVARCHAR(30) NOT NULL
+    ContractTypeID INT IDENTITY(1,1) NOT NULL, 
+    TypeName NVARCHAR(30) NOT NULL
 
 )
 GO
 
 CREATE TABLE [dbo].[Product](
-	ProductID INT IDENTITY(1,1) NOT NULL, 
-	ProductName NVARCHAR(30) NOT NULL
+    ProductID INT IDENTITY(1,1) NOT NULL, 
+    ProductName NVARCHAR(30) NOT NULL
 )
 GO
 
 CREATE TABLE [dbo].[Client](
-	ClientID INT IDENTITY(1,1) NOT NULL, 
-	FullName NVARCHAR(30) NOT NULL
+    ClientID INT IDENTITY(1,1) NOT NULL, 
+    FullName NVARCHAR(30) NOT NULL
 )
 GO
 
 CREATE TABLE [dbo].[Service](
-	ServiceID INT IDENTITY(1,1) NOT NULL, 
-	ServiceName NVARCHAR(30) NOT NULL, 
-	StartDate DATE NOT NULL, 
-	EndDate DATE NOT NULL
+    ServiceID INT IDENTITY(1,1) NOT NULL, 
+    ServiceName NVARCHAR(30) NOT NULL, 
+    StartDate DATE NOT NULL, 
+    EndDate DATE NOT NULL
 )
 GO
 
 CREATE TABLE [dbo].[mapContractClient](
-	ContractID INT NOT NULL, 
-	ClientID INT NOT NULL
+    ContractID INT NOT NULL, 
+    ClientID INT NOT NULL
 )
 GO
 
 CREATE TABLE [dbo].[mapContractService](
-	ContractID INT NOT NULL, 
-	ServiceID INT NOT NULL
+    ContractID INT NOT NULL, 
+    ServiceID INT NOT NULL
 )
 GO
 
 CREATE TABLE [dbo].[mapServiceProduct](
-	ServiceID INT NOT NULL,
-	ProductID INT NOT NULL
+    ServiceID INT NOT NULL,
+    ProductID INT NOT NULL
 )
 GO
 
